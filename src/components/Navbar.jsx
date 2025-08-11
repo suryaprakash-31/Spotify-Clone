151114296e081cf15676d1a68fafa13b762f8dd4
suryaprakash-31 <158264455+suryaprakash-31@users.noreply.github.com>
2025-08-11 18:55:29 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,7 +21,7 @@ const Navbar = () => {
      <div className='flex items-center gap-2 mt-4'>
             <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'>All</p>
             <p className='bg-black text-white px-4 py-1 rounded-2xl cursor-pointer '>Music</p>
-            <p className='bg-black text-white px-4 py-1 rounded-2xl cursor-pointer '>Music</p>
+            <p className='bg-black text-white px-4 py-1 rounded-2xl cursor-pointer '>Albums</p>
 
         </div>
     </>
